1b95c647e3457dfccee3cd065bb8193ede311396
lobamo <loba6803@gmail.com>
2018-11-01 17:03:15 +0100

    wikipedia

---
+++ app.js
@@ -0,0 +1,45 @@
+const form = document.querySelector('.searchForm');
+
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event){
+	//prevent page from reload
+	event.preventDefault();
+	//get the value of the input field
+	const input = document.querySelector('.searchForm-input').value;
+	//remove whitespace from the input
+	const searchQuery = input.trim();
+	//print searchQuery in the console
+	fetchResults(searchQuery);
+}
+
+function fetchResults(searchQuery){
+	const endpoint = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=${searchQuery}`
+	
+	fetch(endpoint)
+	.then(response => response.json())
+	.then(data => {
+		const results = data.query.search;
+		displayResults(results)
+	})
+	.catch(() => console.log('an error occured'))
+}
+
+function displayResults(results){
+	const searchResults = document.querySelector('.searchResults');
+	searchResults.innerHtML = ''
+
+	results.forEach(result => {
+		const url = encodeURI(`https://en.wikipedia.org/wiki/${result.title}`)
+	
+		searchResults.insertAdjacentHTML('beforeend',
+			`<div class="resultItem">
+			<h3 class="resultItem-title">
+			<a href="${url}" target="_blank" rel="noopener">${result.title}</a>
+			</h3>
+			<span class="resultItem-snippet">${result.snipper}</span></br>
+			<a href="${url}" class="resultItem-link" target="_blank" rel="noopener">${url}</a>
+			</div>`
+			)
+	})
+}
